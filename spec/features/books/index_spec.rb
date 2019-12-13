ee742661c8e5546d481655f3a71fe93207c8782f
DeMarcus Kirby <tkd024@gmail.com>
2019-12-13 13:42:27 -0700

    Add iteration of authors to test for books_two

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(book.title)
       expect(page).to have_content(book.pages)
       expect(page).to have_content(book.publication_year)
-      book.
-      expect(page).to have_content(book.publication_year)
+      expect(page).to have_content(book.authors)
     end
 
     expect(page).to_not have_content("Battle of Kat")
     expect(page).to_not have_content("500")
     expect(page).to_not have_content("2009")
+    expect(page).to_not have_content(author_two)
 
     book_two = author.books.create!({title: "Battle of Kat", pages: 500, publication_year: "2009"})
     book_two.authors << author_two
@@ -29,12 +29,16 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(book.title)
       expect(page).to have_content(book.pages)
       expect(page).to have_content(book.publication_year)
+      expect(page).to have_content(book.authors)
     end
 
     within("#book-#{book_two.id}") do
       expect(page).to have_content(book_two.title)
       expect(page).to have_content(book_two.pages)
       expect(page).to have_content(book_two.publication_year)
+      book_two.authors.each do |author|
+        expect(page).to have_content(author)
+      end
     end
   end
 end
