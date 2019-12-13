f2f3ee616c358c0925dac36df83bedf6d199cebd
DeMarcus Kirby <tkd024@gmail.com>
2019-12-13 13:55:13 -0700

    Update view to reflect test expectations

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -12,13 +12,15 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(book.title)
       expect(page).to have_content(book.pages)
       expect(page).to have_content(book.publication_year)
-      expect(page).to have_content(book.authors)
+      book.authors.each do |author|
+        expect(page).to have_content(author.name)
+      end
     end
 
     expect(page).to_not have_content("Battle of Kat")
     expect(page).to_not have_content("500")
     expect(page).to_not have_content("2009")
-    expect(page).to_not have_content(author_two)
+    expect(page).to_not have_content(author_two.name)
 
     book_two = author.books.create!({title: "Battle of Kat", pages: 500, publication_year: "2009"})
     book_two.authors << author_two
@@ -29,7 +31,9 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(book.title)
       expect(page).to have_content(book.pages)
       expect(page).to have_content(book.publication_year)
-      expect(page).to have_content(book.authors)
+      book.authors.each do |author|
+        expect(page).to have_content(author.name)
+      end
     end
 
     within("#book-#{book_two.id}") do
@@ -37,7 +41,7 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(book_two.pages)
       expect(page).to have_content(book_two.publication_year)
       book_two.authors.each do |author|
-        expect(page).to have_content(author)
+        expect(page).to have_content(author.name)
       end
     end
   end
