279cefec76f82c5931706b2758f302bd572b749c
DeMarcus Kirby <tkd024@gmail.com>
2019-12-13 11:46:05 -0700

    Add author model test suite

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :name}
+  end
+
+  describe 'relationships' do
+    it {should have_many :authors_books}
+    it {should have_many(:books).through(:authors_books)}
+  end
+end
