4f0a22ee812afc33074c1db5b2dd1b08e6cff3bb
DeMarcus Kirby <tkd024@gmail.com>
2019-12-13 13:37:50 -0700

    Add authors to test for books index

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor', type: :feature do
   it 'Should show all books' do
-    book = Book.create!({title: "Life of Tron", pages: 100, publication_year: "2014"})
+    author = Author.create!({name: "Tron"})
+    author_two = Author.create!({name: "Kat"})
+    book = author.books.create!({title: "Life of Tron", pages: 100, publication_year: "2014"})
 
     visit '/books'
 
@@ -10,13 +12,16 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(book.title)
       expect(page).to have_content(book.pages)
       expect(page).to have_content(book.publication_year)
+      book.
+      expect(page).to have_content(book.publication_year)
     end
 
     expect(page).to_not have_content("Battle of Kat")
     expect(page).to_not have_content("500")
     expect(page).to_not have_content("2009")
 
-    book_two = Book.create!({title: "Battle of Kat", pages: 500, publication_year: "2009"})
+    book_two = author.books.create!({title: "Battle of Kat", pages: 500, publication_year: "2009"})
+    book_two.authors << author_two
 
     visit '/books'
 
